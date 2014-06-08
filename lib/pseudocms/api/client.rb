68f80d40a0891999dce4a9ffd2408279bb3c109e
pseudomuto <david.muto@gmail.com>
2014-06-08 19:50:14 -0400

    adding first_page, last_page etc.

--- lib/pseudocms/api/client.rb
+++ lib/pseudocms/api/client.rb
@@ -28,7 +28,12 @@ module PseudoCMS
       end
 
       # has_first_page?, has_prev_page?, has_next_page?, has_last_page?
+      # first_page, prev_page, next_page, last_page
       [:first, :prev, :next, :last].each do |rel|
+        send(:define_method, "#{rel.to_s}_page") do
+          last_response.rels[rel] if last_response
+        end
+
         send(:define_method, "has_#{rel.to_s}_page?") do
           last_response && last_response.rels[rel]
         end
