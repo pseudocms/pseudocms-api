a3e97bac71dd545ee26159058a1ac460be33ae1c
pseudomuto <david.muto@gmail.com>
2014-06-14 11:18:51 -0400

    adding specs for bad credentials

--- spec/pseudocms/api/client/tokens_spec.rb
+++ spec/pseudocms/api/client/tokens_spec.rb
@@ -7,7 +7,7 @@ describe PseudoCMS::API::Client::Tokens do
     context "when using basic auth" do
       let(:client) { basic_auth_client }
 
-      it "creates a new access token" do
+      it "creates a new access token with valid credentials" do
         token = client.create_token(
           client_id: test_api_client_id,
           client_secret: test_api_client_secret
@@ -16,7 +16,28 @@ describe PseudoCMS::API::Client::Tokens do
         expect(token).to_not be_nil
         expect(token.access_token).to_not be_nil
         expect(token.token_type).to eql('bearer')
-        #assert_requested :post, api_url('/oauth/token')
+      end
+
+      it "fails to create an access token with bad user credentials" do
+        client.instance_variable_set("@password", "bad_password_value!")
+
+        token = client.create_token(
+          client_id: test_api_client_id,
+          client_secret: test_api_client_secret
+        )
+
+        expect(token.error).to eql('invalid_resource_owner')
+        expect(client.last_response.status).to be(401)
+      end
+
+      it "fails to create an access token with bad client credentials" do
+        token = client.create_token(
+          client_id: test_api_client_id,
+          client_secret: 'bad_secret_value'
+        )
+
+        expect(token.error).to eql('invalid_client')
+        expect(client.last_response.status).to be(401)
       end
     end
   end
